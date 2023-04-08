d4b95d3c1bf230ff79d10af65dfa8dee4f61d87c
Christina dt <cnicodc@gmail.com>
2023-04-08 12:58:44 -0700

    play pause ff controls working

--- js/card_gen.js
+++ js/card_gen.js
@@ -7,7 +7,7 @@ const cards =[
     },
     desription: {
         text: "4 Bell Peppers\n 1 lb ground beef \n 1 cup cooked rice",
-    }
+    },
 },
 {
     title: "Time to Cook",
@@ -17,6 +17,22 @@ const cards =[
     },
     desription: {
         text: "Prep Time: 15min\n Cook Time: 1 hr 30 mins\n Total Time: 1 hr 45mins\n Servings: 4"
-    }
-}
+    },
+},
 ];
+
+const container = document.getElementById("bottom-cards");
+const template_card = document.getElementsByClassName("info-card")[0];
+
+cards.forEach((data) => {
+    const card = template_card.cloneNode(true);
+    card.style.display = "";
+    const header = card.children[0];
+    const image = card.children[1];
+    const description = card.children[2];
+    header.textContent = data.title;
+    image.src = data.icon.src;
+    image.alt = data.icon.alt;
+    description.textContent = data.description.text;
+    container.appendChild(card);
+});
