8e61213936dee2bc1361ebafb3dfb58194aae9f1
Christina dt <cnicodc@gmail.com>
2023-04-08 10:29:28 -0700

    videos and form

---
+++ js/card_gen.js
@@ -0,0 +1,22 @@
+const cards =[
+{
+    title: "Ingredients",
+    icon: {
+        src: "",
+        alt: "",
+    },
+    desription: {
+        text: "4 Bell Peppers\n 1 lb ground beef \n 1 cup cooked rice",
+    }
+},
+{
+    title: "Time to Cook",
+    icon: {
+        src:"",
+        alt:"",
+    },
+    desription: {
+        text: "Prep Time: 15min\n Cook Time: 1 hr 30 mins\n Total Time: 1 hr 45mins\n Servings: 4"
+    }
+}
+];
